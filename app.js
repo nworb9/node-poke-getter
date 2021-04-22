9bb9245a9d27f2c2c21ffb39efa46c370a74a700
Emma Brown <nworb95@gmail.com>
2021-04-22 10:58:54 -0400

    Added habitat test request

--- app.js
+++ app.js
@@ -5,8 +5,15 @@ const TYPE_ENDPOINT = 'https://pokeapi.co/api/v2/type/'
 const HABITAT_ENDPOINT = 'https://pokeapi.co/api/v2/pokemon-habitat/'
 
 const TEST_TYPE = 'flying'
+const TEST_HABITAT = 'cave'
 
 request(TYPE_ENDPOINT + TEST_TYPE + '/', function (error, response, body) {
+    if (!error && response.statusCode == 200){
+        console.log(JSON.parse(body))
+    }
+})
+
+request(HABITAT_ENDPOINT + TEST_HABITAT + '/', function (error, response, body) {
     if (!error && response.statusCode == 200){
         console.log(JSON.parse(body))
     }
