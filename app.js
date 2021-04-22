043a86a69bce09bae3084af65fe84c3af30d7201
Emma Brown <nworb95@gmail.com>
2021-04-22 14:54:56 -0400

    Added PokemonCatcher and replacing request with bent

--- app.js
+++ app.js
@@ -1,21 +1,14 @@
-import request from 'request';
+var PokemonCatcher = require('./src/PokemonCatcher.js');
 
-const typeEndpoint = 'https://pokeapi.co/api/v2/type/'
-const habitatEndpoint = 'https://pokeapi.co/api/v2/pokemon-habitat/'
-
-const TEST_TYPE = 'flying'
-const TEST_HABITAT = 'cave'
+const TEST_TYPE = 'flying';
+const TEST_HABITAT = 'cave';
 
 // based on this tutorial https://www.vikingcodeschool.com/professional-development-with-javascript/writing-the-api-wrapper
 
-request(typeEndpoint + TEST_TYPE + '/', function (error, response, body) {
-    if (!error && response.statusCode == 200){
-        console.log(JSON.parse(body))
-    }
-})
+const pokecatcher = new PokemonCatcher(TEST_HABITAT, TEST_TYPE);
 
-request(habitatEndpoint + TEST_HABITAT + '/', function (error, response, body) {
-    if (!error && response.statusCode == 200){
-        console.log(JSON.parse(body))
-    }
+console.log(pokecatcher)
+
+pokecatcher.getPokemonByType(function(data) {
+    console.log(data);
 })
