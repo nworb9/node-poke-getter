0bc14d11d6190b4b1d9c9d0dce5f0433f4583742
nworb9 <emma.nworb@gmail.com>
2021-05-10 10:08:45 -0400

    Added getTypePokemon and removed PokemonCatcher class entirely

--- app.js
+++ app.js
@@ -1,22 +1,43 @@
-var PokemonCatcher = require('./src/PokemonCatcher.js');
-const fetch = require('node-fetch');
+const fs = require('fs');
+const axios = require('axios');
 
+
+const MEDIA_DIR = './media'; // is there a good file to put constants stored?
+const TYPE_ENDPOINT = 'https://pokeapi.co/api/v2/type/';
+const HABITAT_ENDPOINT = 'https://pokeapi.co/api/v2/pokemon-habitat/';
 const TEST_TYPE = 'flying';
 const TEST_HABITAT = 'cave';
 
 // based on this tutorial https://www.vikingcodeschool.com/professional-development-with-javascript/writing-the-api-wrapper
 
-const pokecatcher = new PokemonCatcher(TEST_HABITAT, TEST_TYPE);
-
-async function catchPokemon() {
-    try {
-        pokecatcher.init().then(val => {
-            console.log("success");
-            return val + 1;
-        }, console.log).then(console.log)
-    } catch (err) {
-        console.log(err)
-    }
+if (!fs.existsSync(MEDIA_DIR)){
+    fs.mkdirSync(MEDIA_DIR);
 }
 
-catchPokemon()
+async function sendRequest(endpoint, filter) { // at what point does it make sense to name a specific function call ?
+    try {
+        const url = endpoint + filter + '/';
+        const response = await axios.get(url);
+        return response.data;
+    } catch (error) {
+        console.log(error);
+    }   
+}
+
+async function getTypePokemon(type) {
+    const typeResponse = await sendRequest(TYPE_ENDPOINT, type);
+    const typePokemon = typeResponse.pokemon;
+    return typePokemon.map((pokemon) => {
+        return {
+            'name': pokemon['pokemon']['name'],
+            'endpoint': pokemon['pokemon']['url']
+        }});
+}
+
+async function getHabitatPokemon(habitat) {
+    return sendRequest(HABITAT_ENDPOINT, habitat);
+}
+
+const typePokemon = getTypePokemon(TEST_TYPE).then(console.log);
+
+
