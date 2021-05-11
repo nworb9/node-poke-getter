8a7bbef51b62e270e5bf6b59086221fba2668ce6
nworb9 <emma.nworb@gmail.com>
2021-05-10 23:38:49 -0400

    Finished handling for missing params

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const fs = require("fs");
 const axios = require("axios");
+const { type } = require("os");
 
 const MEDIA_DIR = "./media"; // is there a good file to put constants stored?
 const TYPE_ENDPOINT = "https://pokeapi.co/api/v2/type/";
 const HABITAT_ENDPOINT = "https://pokeapi.co/api/v2/pokemon-habitat/";
 const TYPE = "dark";
-const HABITAT = "cave";
+const HABITAT = "urban";
 
 if (!fs.existsSync(MEDIA_DIR)) {
   fs.mkdirSync(MEDIA_DIR);
@@ -45,33 +46,56 @@ async function getSprite(pokemon) {
 }
 
 async function getTypePokemon(type) {
-  const typeResponse = await requestPokemon(TYPE_ENDPOINT, type).catch((e) => {
-    console.log(e);
-  });
-  const typePokemon = typeResponse.pokemon;
-  return typePokemon.map((pokemon) => ({
-    name: pokemon["pokemon"]["name"],
-    endpoint: pokemon["pokemon"]["url"],
-  }));
+  if (type) {
+    const typeResponse = await requestPokemon(TYPE_ENDPOINT, type).catch(
+      (e) => {
+        console.log(e);
+      }
+    );
+    const typePokemon = typeResponse.pokemon;
+    return typePokemon.map((pokemon) => ({
+      name: pokemon["pokemon"]["name"],
+      endpoint: pokemon["pokemon"]["url"],
+    }));
+  } else {
+    return [];
+  }
 }
 
 async function getHabitatPokemon(habitat) {
-  const habitatResponse = await requestPokemon(HABITAT_ENDPOINT, habitat).catch(
-    (e) => {
+  if (habitat) {
+    const habitatResponse = await requestPokemon(
+      HABITAT_ENDPOINT,
+      habitat
+    ).catch((e) => {
       console.log(e);
-    }
-  );
-  const habitatPokemon = habitatResponse.pokemon_species;
-  return habitatPokemon.map((pokemon) => ({
-    name: pokemon["name"],
-    endpoint: pokemon["url"].replace("pokemon-species", "pokemon"),
-  }));
+    });
+    const habitatPokemon = habitatResponse.pokemon_species;
+    return habitatPokemon.map((pokemon) => ({
+      name: pokemon["name"],
+      endpoint: pokemon["url"].replace("pokemon-species", "pokemon"),
+    }));
+  } else {
+    return [];
+  }
 }
 
 function filterPokemon(type_pokemon, habitat_pokemon) {
-  return type_pokemon.filter((pokemon) =>
-    habitat_pokemon.find(({ name }) => pokemon.name === name)
-  );
+    console.log(type_pokemon);
+    console.log(habitat_pokemon);
+    if (type_pokemon.length !== 0 && habitat_pokemon.length !== 0) {
+        console.log(1);
+        return type_pokemon.filter((pokemon) =>
+        habitat_pokemon.find(({ name }) => pokemon.name === name)
+      );    
+    } else if (type_pokemon) {
+        console.log(2);
+        return type_pokemon;
+    } else if (habitat_pokemon) {
+        console.log(3);
+        return habitat_pokemon;
+    }
+  
 }
 
 async function getPokemon(type, habitat) {
@@ -80,16 +104,18 @@ async function getPokemon(type, habitat) {
   const pokemonList = filterPokemon(typePokemon, habitatPokemon);
   const finalPokemon = await Promise.all(
     pokemonList.map(async (poke) => {
-       const sprite = await getSprite(poke).catch((e) => {console.log(e)});
-       return {
-         name: poke["name"],
-         sprite: sprite,
-       }
-    }
-  ));
+      const sprite = await getSprite(poke).catch((e) => {
+        console.log(e);
+      });
+      return {
+        name: poke["name"],
+        sprite: sprite,
+      };
+    })
+  );
   return finalPokemon;
 }
 
-// add handling for when there isn't a type/habitat specified
+// TODO add handling for when there isn't a type/habitat specified
 
 const pokemon = getPokemon(TYPE, HABITAT).then(console.log);
